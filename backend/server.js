8d394674e02c6fa1f46d6df70cc6b07a8f432897
Thiago Lins <thiago.jorge2001@gmail.com>
2025-05-02 05:45:05 -0300

    Adicionando prometheus para coleta de métricas e o grafana para visualização

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,32 @@ const PORT = 3000;
 const cors = require('cors');
 const postRoutes = require('./routes/postRoutes');
 const requestLogger = require('./middleware/requestLogger');
+const client = require('prom-client');
+const register = new client.Registry();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Contador total de requisições HTTP',
+  labelNames: ['method', 'route', 'serverId']
+});
+
+register.registerMetric(httpRequestCounter);
+register.setDefaultLabels({ app: 'load_balancer_demo' });
+client.collectDefaultMetrics({ register });
+
+app.use((req, res, next) => {
+  httpRequestCounter.inc({
+    method: req.method,
+    route: req.path,
+    serverId: SERVER_ID
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 const SERVER_ID = process.env.SERVER_ID;
 
